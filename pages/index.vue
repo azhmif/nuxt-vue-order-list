dfb77e66d4ea0ed33bf0a26d791f3f6ff483f7b6
azhmif <dev.azhmif@gmail.com>
2024-09-25 13:43:45 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <nav class="navbar">
+      <div class="logo_item">
+        <i class="bx bx-menu" id="sidebarOpen"></i>
+        <img src="/img/brand_seakun.svg" alt="brand seakun">
+      </div>
+      
+    </nav>
+    <nav class="sidebar">
+      <div class="menu_content">
+        <ul class="menu_items">
+          <li class="item">
+            <div href="#" class="nav_link submenu_item">
+              
+              <span class="navlink">Order List</span>
+            
+            </div>
+          </li>
+          </ul>
+          </div>
+
+    </nav>
+
+    <div class="content-wrapper">
+        <div class="content">
+            <table>
+                <thead>
+                    <tr>
+                        <th>sadas</th>
+                        <th>sadas</th>
+                        <th>sadas</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>asdas</td>
+                        <td>asdas</td>
+                        <td>asdas</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+
+    </div>
+  </div>
+</template>
+
+
